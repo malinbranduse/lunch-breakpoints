1f73570a5a5a9e6493399c9b3c382a07e90ea575
Mălin Brândușe <malin.branduse@gmail.com>
2017-04-30 13:16:25 +0300

    indent to 4 spaces

--- simple-breakpoints.js
+++ simple-breakpoints.js
@@ -5,147 +5,144 @@
  */
 
 ;(function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define([], factory);
-  } else if (typeof module === 'object' && module.exports) {
-    // Node.js
-    module.exports = factory();
-  } else {
-    // Browser globals (root is window)
-    root.returnExports = factory();
-  }
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node.js
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+    }
 }(this, function () {
 
-  var defaults = {
-    throttle: false,
-    mobileFirst: false
-  };
+    var defaults = {
+        throttle: false,
+        mobileFirst: false
+    };
 
   // Function constructor
-  function SimpleBreakpoints(opts){
-    this._ = extend(defaults, opts);
-    if (!this._.hasOwnProperty('default') || !this._.hasOwnProperty('breakpoints')) return;
+    function SimpleBreakpoints(opts){
+        this._ = extend(defaults, opts);
+        if (!this._.hasOwnProperty('default') || !this._.hasOwnProperty('breakpoints')) return;
 
-    this._init();
-  }
-
-  SimpleBreakpoints.prototype._init = function() {
-    let self = this,
-      breakpoints = this._.breakpoints,
-      breakpointsArray = [];
-
-    // Put all breakpoints inside an array
-    for (let breakpoint in breakpoints) {
-      if( breakpoints.hasOwnProperty( breakpoint ) ) {
-        breakpointsArray.push(parseInt(breakpoint));
-      }
+        this._init();
     }
 
-    // Sort the array - highest breakpoint first
-    this.breakpointsArray = breakpointsArray.sort(function(a,b) { return (b-a) });
-    this.activeBreakpoint = 'default';
+    SimpleBreakpoints.prototype._init = function() {
+        var self = this,
+            breakpoints = this._.breakpoints,
+            breakpointsArray = [];
 
-    self._getActiveBreakpoint();
-    self._changedBreakpoint();
+        // Put all breakpoints inside an array
+        for (var breakpoint in breakpoints)
+            breakpoints.hasOwnProperty(breakpoint) &&
+                breakpointsArray.push(parseInt(breakpoint))
 
-    if (typeof self._.throttle === 'number')
-      window.addEventListener('resize', throttle(function() {
+        // Sort the array - highest breakpoint first
+        this.breakpointsArray = breakpointsArray.sort(function(a,b) { return (b-a) });
+        this.activeBreakpoint = 'default';
+
         self._getActiveBreakpoint();
-      }, self._.throttle));
-    else
-      window.addEventListener('resize', self._getActiveBreakpoint.bind(self));
+        self._changedBreakpoint();
+    };
 
-  };
+    SimpleBreakpoints.prototype._bindEvents = function() {
+        var self = this,
+            throttle = typeof self._.throttle === 'number';
 
-  SimpleBreakpoints.prototype._getActiveBreakpoint = function() {
-    var w = window.innerWidth,
-      bps = this.breakpointsArray,
-      lastBp = this.activeBreakpoint,
-      activeBp = 'default';
+        var resizeHandler = throttle
+            ? throttle(self._getActiveBreakpoint.bind(self), self._.throttle)
+            : self._getActiveBreakpoint.bind(self);
 
-    // breakpoints are sorted so it's safe to use the following method for determining the current interval
-    // Mobile first
-    if (this._.mobileFirst)
-      for (var i = 0; i < bps.length; i++)
-        w > bps[i] && (activeBp = bps[i]);
-    else
-    // Desktop first
-      for (var i = 0; i < bps.length; i++)
-        w < bps[i] && (activeBp = bps[i]);
+        window.addEventListener('resize', resizeHandler);
+    };
 
-    this.activeBreakpoint = activeBp;
+    SimpleBreakpoints.prototype._getActiveBreakpoint = function() {
+        var self = this,
+            w = window.innerWidth,
+            bps = this.breakpointsArray,
+            lastBp = this.activeBreakpoint,
+            activeBp = 'default',
+            mobileCondition = function(bp) {
+                return self._.mobileFirst ? w > bp : w < bp;
+            };
 
-    if (activeBp !== lastBp)
-      this._changedBreakpoint();
+        // breakpoints are sorted so it's safe to use the following method for determining the current interval
+        for (var i = 0; i < bps.length; i++)
+            mobileCondition(bps[i]) && (activeBp = bps[i]);
 
-    return this.activeBreakpoint;
-  };
+        this.activeBreakpoint = activeBp;
 
-  SimpleBreakpoints.prototype._changedBreakpoint = function() {
+        if (activeBp !== lastBp)
+            this._changedBreakpoint();
+
+        return this.activeBreakpoint;
+    };
+
+    SimpleBreakpoints.prototype._changedBreakpoint = function() {
 
     // call breakpoint functions
     if (this._.breakpoints.hasOwnProperty(this.activeBreakpoint))
 
-      this._.breakpoints[this.activeBreakpoint].call(this);
+        this._.breakpoints[this.activeBreakpoint].call(this);
 
     else if (this.activeBreakpoint === 'default')
 
-      this._.default.call(this);
+        this._.default.call(this);
 
-  };
+    };
 
   // Helper functions
 
-  function extend( a, b ) {
-    for( var key in b ) {
-      if( b.hasOwnProperty( key ) ) {
-        a[key] = b[key];
-      }
+    function extend( a, b ) {
+        for( var key in b )
+            b.hasOwnProperty(key) &&
+                (a[key] = b[key]);
+        return a;
     }
-    return a;
-  }
 
-  function throttle(func, wait, options) {
-    var timeout, context, args, result;
-    var previous = 0;
-    if (!options) options = {};
+    function throttle(func, wait, options) {
+        var timeout, context, args, result;
+        var previous = 0;
+        if (!options) options = {};
 
-    var later = function() {
-      previous = options.leading === false ? 0 : Date.now();
-      timeout = null;
-      result = func.apply(context, args);
-      if (!timeout) context = args = null;
-    };
+        var later = function() {
+            previous = options.leading === false ? 0 : Date.now();
+            timeout = null;
+            result = func.apply(context, args);
+            if (!timeout) context = args = null;
+        };
 
-    var throttled = function() {
-      var now = Date.now();
-      if (!previous && options.leading === false) previous = now;
-      var remaining = wait - (now - previous);
-      context = this;
-      args = arguments;
-      if (remaining <= 0 || remaining > wait) {
-        if (timeout) {
-          clearTimeout(timeout);
-          timeout = null;
-        }
-        previous = now;
-        result = func.apply(context, args);
-        if (!timeout) context = args = null;
-      } else if (!timeout && options.trailing !== false) {
-        timeout = setTimeout(later, remaining);
-      }
-      return result;
-    };
+        var throttled = function() {
+            var now = Date.now();
+            if (!previous && options.leading === false) previous = now;
+            var remaining = wait - (now - previous);
+            context = this;
+            args = arguments;
+            if (remaining <= 0 || remaining > wait) {
+                if (timeout) {
+                    clearTimeout(timeout);
+                    timeout = null;
+                }
+                previous = now;
+                result = func.apply(context, args);
+                if (!timeout) context = args = null;
+            } else if (!timeout && options.trailing !== false) {
+                timeout = setTimeout(later, remaining);
+            }
+            return result;
+        };
 
-    throttled.cancel = function() {
-      clearTimeout(timeout);
-      previous = 0;
-      timeout = context = args = null;
-    };
+        throttled.cancel = function() {
+            clearTimeout(timeout);
+            previous = 0;
+            timeout = context = args = null;
+        };
 
-    return throttled;
-  }
+        return throttled;
+    }
 
   return SimpleBreakpoints;
 }));
